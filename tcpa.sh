54ae44a2574e767b83c638850f10fb0207a3bf6b
wangzhe12588 <wangzhe12588@gmail.com>
2019-04-04 22:18:10 +0800

    Update tcpa.sh

--- tcpa.sh
+++ tcpa.sh
@@ -7,7 +7,7 @@ export LANG=en_US.UTF-8
 yum -y install epel-release
 yum -y install net-tools
 
-curl -o /tmp/tcpa_packets_180619_1151.tar.gz http://down.08mb.com/tcp_opz/tcpa/tcpa_packets_180619_1151.tar.gz
+curl -o /tmp/tcpa_packets_180619_1151.tar.gz https://github.com/wangzhe12588/tcpa/raw/master/tcpa_packets_180619_1151.tar.gz
 chmod +x /etc/rc.d/rc.local
 cat>>/etc/rc.local<<EOF
 ####tcpa####
@@ -26,7 +26,11 @@ cd /usr/local/storage/tcpav2
 sh start.sh
 EOF
 
-rpm -ivh http://down.08mb.com/tcp_opz/tcpa/kernel-3.10.0-693.5.2.tcpa06.tl2.x86_64.rpm --force
+yum -y install net-tools && yum install -y unzip zip
+wget --no-check-certificate https://raw.githubusercontent.com/wangzhe12588/tcpa/master/kernel-3.10.0-693.5.2.tcpa06.tl2.x86_64.zip && wget --no-check-certificate https://raw.githubusercontent.com/wangzhe12588/tcpa/master/kernel-3.10.0-693.5.2.tcpa06.tl2.x86_64.z01
+cat kernel-3.10.0-693.5.2.tcpa06.tl2.x86_64.z*>kernel-3.10.0-693.5.2.zip
+unzip kernel-3.10.0-693.5.2.zip
+rpm -ivh kernel-3.10.0-693.5.2.tcpa06.tl2.x86_64.rpm --force
 echo "内核安装完毕,3秒后将自动重启..."
 echo "重启后安装自动完成,lsmod|grep tcpa查看是否开启成功."
 sleep 3
